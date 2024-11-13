9da6404c85aa4f6c6df74e2d137c161dfc4ec6b8
GreenfourmR <Monhbayr2400@gmail.com>
2024-11-13 12:57:30 +0800

    update

--- back-end/index.js
+++ back-end/index.js
@@ -1,10 +1,14 @@
-import express, { response } from "express";
+import express from "express";
+import cors from "cors";
 
 const server = express();
 const PORT = 8000;
 
+server.use(cors());
 server.get("/", (request, response) => {
-  response.send("Hello world");
+  response.json({
+    success: "successfully",
+  });
 });
 
 server.listen(PORT, () => {
--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,19 +1,22 @@
 import Image from "next/image";
+import { useEffect } from "react";
 
 export default function Home() {
+  const fetchData = async () => {
+    try {
+      const response = await fetch(`${process.env.DATABASE_URL}`);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <div>Food Delivery Project</div>
-      </main>
+      <main></main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
