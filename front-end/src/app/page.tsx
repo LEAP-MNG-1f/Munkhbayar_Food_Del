96e4477506393933a942c66dcf13aa096ecf5249
GreenfourmR <Monhbayr2400@gmail.com>
2024-11-14 12:45:22 +0800

    update MUI

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,5 +1,10 @@
+"use client";
 import Image from "next/image";
+import { Router } from "next/router";
 import { useEffect } from "react";
+import { AppProvider } from "@toolpad/core/AppProvider";
+import { SignInPage, type AuthProvider } from "@toolpad/core/SignInPage";
+import { useTheme } from "@mui/material/styles";
 
 export default function Home() {
   const fetchData = async () => {
@@ -14,56 +19,28 @@ export default function Home() {
   useEffect(() => {
     fetchData();
   }, []);
+  const providers = [
+    { id: "github", name: "GitHub" },
+    { id: "google", name: "Google" },
+    { id: "facebook", name: "Facebook" },
+    { id: "twitter", name: "Twitter" },
+    { id: "linkedin", name: "LinkedIn" },
+  ];
+  // preview-end
+  const theme = useTheme();
+
+  const signIn: (provider: AuthProvider) => void = async (provider) => {
+    const promise = new Promise<void>((resolve) => {
+      setTimeout(() => {
+        console.log(`Sign in with ${provider.id}`);
+        resolve();
+      }, 500);
+    });
+    return promise;
+  };
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main></main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <AppProvider theme={theme}>
+      <SignInPage signIn={signIn} providers={providers} />
+    </AppProvider>
   );
 }
