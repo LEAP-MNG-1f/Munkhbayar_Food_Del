4ec1760f50f3a9aa76a3620f12e5a2229eafe3d4
GreenfourmR <monhbayr2400@gmail.com>
2024-12-03 10:13:46 +0800

    front add

--- front-end/src/app/_components/menu/littlebox/littlebox.tsx
+++ front-end/src/app/_components/menu/littlebox/littlebox.tsx
@@ -1,6 +1,9 @@
 import CloseIcon from "@mui/icons-material/Close";
 import AddBoxIcon from "@mui/icons-material/AddBox";
+import { useState } from "react";
+
 export const Littlebox = () => {
+  const [number, setNumber] = useState(0);
   return (
     <div className="flex w-full p-4 gap-3">
       <div className="flex w-full h-full">
@@ -12,22 +15,31 @@ export const Littlebox = () => {
             <h1 className="font-bold text-[20px]">Main Pizza </h1>
             <h1 className="text-[#18BA51] font-semibold">34,800₮</h1>
           </div>
-          <button className="flex justify-center items-center hover:bg-slate-200 rounded-xl p-2">
+          <div className="flex justify-center items-center hover:bg-slate-200 rounded-xl p-2">
             <CloseIcon />
-          </button>
+          </div>
         </div>
         <div className="flex items-center text-[20px] text-[#767676]">
           Хулуу, төмс, лууван, сонгино, цөцгийн тос, самрын үр{" "}
         </div>
         <div className="flex w-[50%] justify-between items-end">
           <button className="flex rounded-md w-[40px] bg-[#18BA51] text-[white] text-[25px]">
-            <p className="w-full h-full flex justify-center items-center">-</p>
+            <button
+              className="w-full h-full flex justify-center items-center"
+              onClick={() => setNumber(number + 1)}
+            >
+              +
+            </button>
           </button>
           <div className="flex justify-center items-center h-full">
-            <p className="flex justify-center items-center ">1</p>
+            {number}
           </div>
-          <button className="flex justify-center items-center w-[40px] rounded-md bg-[#18BA51] text-[white] text-[25px]">
-            <p className="flex justify-center items-center">+</p>
+
+          <button
+            className="flex justify-center items-center w-[40px] rounded-md bg-[#18BA51] text-[white] text-[25px]"
+            onClick={() => setNumber(number - 1)}
+          >
+            -
           </button>
         </div>
       </div>
